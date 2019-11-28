1ce32d017da77b52ed1a226dbec254f394054609
Anshoo Mishra <anshum45@gmail.com>
2019-11-29 01:10:38 +0530

    com.xtreme.eims.lessonplaning.pdfgeneration

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.xtremesoftech.eims.lessonplanning.pdfgeneration;
+import com.xtremesoftech.eims.lessonplanning.pdfgeneration.PdfGeneration;
+public class Test {
+
+	public static void main(String[] args) {
+		PdfGeneration pdfgen = new PdfGeneration();
+		pdfgen.generatePdf();
+
+	}
+
+}
